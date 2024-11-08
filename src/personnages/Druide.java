70aba9338edc19c6dbfc575ff991e289da5b4d72
YaaaPaaa <yann.paluch@univ-tlse3.fr>
2024-11-08 10:56:13 +0100

    Ajout de l'Exercice 3

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,9 +1,12 @@
 package personnages;
 
+import java.util.Random;
+
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+	private int forcePotion = 1;
 	
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
@@ -22,5 +25,23 @@ public class Druide {
 	
 	private String prendreParole() {
 		return "Le druide " + nom + " : ";
+	}
+	
+	//Exercise [3]a)
+	private void preparerPotion() {
+		Random random = new Random ();
+		forcePotion = random.nextInt(effetPotionMax+1);
+		while(effetPotionMin > forcePotion) //Exercise [3]c)
+			forcePotion = random.nextInt(effetPotionMax+1);
+		if (forcePotion > 7)
+			parler("J'ai préparé une super potion de force " + forcePotion);
+		else
+			parler("Je n'ai pas trouvé tout les ingrédients, ma potion est seulement de force " + forcePotion);
+	}
+	
+	//Exercise [3]b)
+	public static void main(String[] args) {
+		Druide panoramix = new Druide ("Panoramix",5,10);
+		panoramix.preparerPotion();
 	}
 }
